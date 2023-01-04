d2726c0ce111638bf7c495801fbf2c8a49eb604f
JunkieXLL <121937848+JunkieXLL@users.noreply.github.com>
2023-01-04 11:48:06 +0100

    Add files via upload
    My first code in Python

---
+++ band-name-generator.py
@@ -0,0 +1,6 @@
+print('Welcome to the Band Name Generator.')
+city = input('What is of the city you grew up in?\n')
+pet = input('What is your pet is name?\n')
+print('Your ban name could be ' + city, pet)
+
+
