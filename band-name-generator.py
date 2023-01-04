c337ce1907e1b7c648ba7ca78dc23b3886815b3c
JunkieXLL <121937848+JunkieXLL@users.noreply.github.com>
2023-01-04 12:07:04 +0100

    Update band-name-generator.py
    correction of typos

--- band-name-generator.py
+++ band-name-generator.py
@@ -1,6 +1,5 @@
-print('Welcome to the Band Name Generator.')
-city = input('What is of the city you grew up in?\n')
-pet = input('What is your pet is name?\n')
-print('Your ban name could be ' + city, pet)
-
+print("Welcome to the Band Name Generator.")
+city = input("What's of the city you grew up in?\n")
+pet = input("What's your pet name?\n")
+print("Your band name could be " + city, pet)
 
